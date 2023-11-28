37e77e6820fb2a120d63818963a269e22251188f
Soroush <ranjbar.soroush32@gmail.com>
2023-11-28 14:04:36 +0330

    GameLogic.jsx added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import GetData from "./data/GetData"
 import Card from "./components/Card"
+import GameLogic from "./components/GameLogic"
 
 function App() {
   const [imageList, setImageList] = useState([])
@@ -11,40 +12,13 @@ function App() {
   
 
   const handleGameStatus = (id) => {
-    
     const temp = imageList.map((item) => {
       if(item.id === id) {
         item.isClicked = true
       } 
       return item  
     })
-    const clickedObjects = temp.filter(item => item.isClicked && !item.isFind);
-    if (clickedObjects.length <= 2) {
-      setImageList(temp)
-      if (clickedObjects.length === 2) {
-        setTimeout(() => {
-          const areImageSame = clickedObjects[0].imagePath === clickedObjects[1].imagePath
-          let result = []
-          if (areImageSame) {
-            result = temp.map((item) => {
-              if (clickedObjects[0].id === item.id || clickedObjects[1].id === item.id) {
-                item.isFind = true
-              }
-              return item
-            })
-          } else {
-            result = temp.map((item) => {
-              if (clickedObjects[0].id === item.id || clickedObjects[1].id === item.id) {
-                item.isClicked = false
-              }
-              return item
-            })
-          }
-          setImageList(result)
-        }, 800)
-      }
-
-    }
+    GameLogic(temp,setImageList)
   }
 
   return (

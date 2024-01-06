85509d726d0377361b69e16d9b47167e1ba9b1ab
Soroush <ranjbar.soroush32@gmail.com>
2024-01-06 14:51:56 +0330

    The imagePath is changed in GetData

--- src/data/GetData.jsx
+++ src/data/GetData.jsx
@@ -1,18 +1,34 @@
-const imagePath = [
-  '../../public/Cars/benz-sclass.png',
-  '../../public/Cars/benz-sedan.png',
-  '../../public/Cars/benz-suv.png',
-  '../../public/Cars/bmw.png',
-  '../../public/Cars/classic-car-ford.png',
-  '../../public/Cars/ferrari.png',
-  '../../public/Cars/ford.png',
-  '../../public/Cars/lamborgini-huracan.png',
-  '../../public/Cars/lamborgini.png',
-  '../../public/Cars/mini-buick.png',
-  '../../public/Cars/tesla.png',
-  '../../public/Cars/classic-car.png'
-]
+import benzSclass from '../../images/Cars/benz-sclass.png'
+import benzSedan from   '../../images/Cars/benz-sedan.png'
+import benzSUV from   '../../images/Cars/benz-suv.png'
+import bmw from   '../../images/Cars/bmw.png'
+import classisCarFord from   '../../images/Cars/classic-car-ford.png'
+import ferrari from   '../../images/Cars/ferrari.png'
+import ford from   '../../images/Cars/ford.png'
+import LamboHuracan from   '../../images/Cars/lamborgini-huracan.png'
+import Lambo from   '../../images/Cars/lamborgini.png'
+import minuBuick from   '../../images/Cars/mini-buick.png'
+import tesla from   '../../images/Cars/tesla.png'
+import classicCar from   '../../images/Cars/classic-car.png'
 
+// const imagePath = [
+//   '../../images/Cars/benz-sclass.png',
+//   '../../images/Cars/benz-sedan.png',
+//   '../../images/Cars/benz-suv.png',
+//   '../../images/Cars/bmw.png',
+//   '../../images/Cars/classic-car-ford.png',
+//   '../../images/Cars/ferrari.png',
+//   '../../images/Cars/ford.png',
+//   '../../images/Cars/lamborgini-huracan.png',
+//   '../../images/Cars/lamborgini.png',
+//   '../../images/Cars/mini-buick.png',
+//   '../../images/Cars/tesla.png',
+//   '../../images/Cars/classic-car.png'
+// ]
+
+const imagePath = [
+  benzSclass, benzSUV, benzSedan, bmw, ferrari, minuBuick, classicCar, classisCarFord, ford, Lambo, LamboHuracan, tesla
+]
 
 export default function GetData() {
   const shuffle = (array) => { 
